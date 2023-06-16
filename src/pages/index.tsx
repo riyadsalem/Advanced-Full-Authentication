8df7a69408ef363421e37edfbc74f8d9f68af04b
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-06-16 18:47:13 +0300

    Setup tailwind css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1>THIS IS ADVANCED FULL AUTHENTICATION</h1>
+      <h1 className="text-red-700 text-4xl bg-yellow-300">
+        THIS IS ADVANCED FULL AUTHENTICATION
+      </h1>
     </>
   );
 }
