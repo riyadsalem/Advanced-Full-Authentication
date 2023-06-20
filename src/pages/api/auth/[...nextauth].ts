6e6fb1e582edbc397aa0fecf797344a296397404
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-06-20 11:54:30 +0300

    Full Connect to The Database & Setup next auth and testing login process with github

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
+import TwitterProvider from "next-auth/providers/twitter";
+import Auth0Provider from "next-auth/providers/auth0";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID as string,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID as string,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER as string,
+    }),
+  ],
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import { NextPageContext } from "next";
+import { useSession, signOut, getSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
+
   return (
     <>
       <h1 className="text-red-700 text-4xl bg-yellow-300">
-        THIS IS ADVANCED FULL AUTHENTICATION
+        Welcome to the First page
       </h1>
+      <button onClick={() => signOut()}>Sign out</button>
     </>
   );
+}
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx);
+  return {
+    props: {
+      session,
+    },
+  };
 }
