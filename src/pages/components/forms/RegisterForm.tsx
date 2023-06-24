0ea86e6e8071a6a29e2b501bb16cb26129937391
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-06-24 17:44:01 +0300

    React Hook Form Setup

--- src/pages/components/forms/RegisterForm.tsx
+++ src/pages/components/forms/RegisterForm.tsx
@@ -2,9 +2,20 @@ import Link from "next/link";
 import { FC, ReactElement } from "react";
 import Input from "../inputs/input";
 import { CiUser } from "react-icons/ci";
+import { useForm } from "react-hook-form";
 
 interface IRegisterFormProps {}
 const RegisterForm: FC<IRegisterFormProps> = (props): ReactElement => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <div className="w-full px-12 py-4">
       <h2 className="text-center text-2xl font-bold tracking-wider text-gray-800">
@@ -19,14 +30,29 @@ const RegisterForm: FC<IRegisterFormProps> = (props): ReactElement => {
           Sign in
         </Link>
       </p>
-      <form className="my-8 text-sm">
-        <Input
-          name="first_name"
-          label="First name"
-          type="text"
-          icon={<CiUser />}
-          placeholder="example"
-        />
+      <form className="my-8 text-sm" onSubmit={handleSubmit(onSubmit)}>
+        <div className="gap-2 md:flex">
+          <Input
+            name="first_name"
+            label="First name"
+            type="text"
+            icon={<CiUser />}
+            placeholder="example"
+            register={register}
+            error={errors?.last_name?.message}
+            disabled={isSubmitting}
+          />
+          <Input
+            name="last_name"
+            label="Last name"
+            type="text"
+            icon={<CiUser />}
+            placeholder="example"
+            register={register}
+            error={errors?.last_name?.message}
+            disabled={isSubmitting}
+          />
+        </div>
       </form>
     </div>
   );
