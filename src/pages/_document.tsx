f35dd16dd6fcb41ccb9a448fda1063d335cfe718
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-06-24 12:17:59 +0300

    Styling the home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { DefaultSeo } from "next-seo";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <DefaultSeo
+            title="Authentification - Riyad Salem"
+            description="Your page description"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
