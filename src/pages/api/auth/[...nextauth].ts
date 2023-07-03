bbf40bc43d0aa40875b0d7fc7ac7f0923e79d65c
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-07-03 09:56:50 +0300

    Add Credentials Provider To Login By email and password

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,57 @@
-import NextAuth, { Account, Profile, User } from "next-auth";
+import NextAuth, {
+  Account,
+  Awaitable,
+  Profile,
+  RequestInternal,
+  User,
+} from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import DiscordProvider from "next-auth/providers/discord";
 import TwitterProvider from "next-auth/providers/twitter";
 import Auth0Provider from "next-auth/providers/auth0";
+import CredentialsProvider from "next-auth/providers/credentials";
+
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 import { Adapter } from "next-auth/adapters";
 import { JWT } from "next-auth/jwt";
+import connectDb from "@/utils/connectDb";
+import UserModal from "@/models/User";
+import bcrypt from "bcryptjs";
 
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Name",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        await connectDb();
+        const user = await UserModal.findOne({ email: credentials!.email });
+        if (!user) {
+          throw new Error("Email is not registered.");
+        }
+        const isPasswordCorrect = await bcrypt.compare(
+          credentials!.password,
+          user.password
+        );
+        if (!isPasswordCorrect) {
+          throw new Error("Password is incorrect.");
+        }
+        return user;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
