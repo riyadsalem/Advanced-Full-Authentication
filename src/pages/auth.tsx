4d30231c6486976ea1c87f321d330c0ecf325b2b
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-07-03 17:13:19 +0300

    Adding our custom providers

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,18 +1,27 @@
 import Background from "@/components/Backgrounds/Background";
+import SocialButton from "@/components/buttons/SocialButton";
 import LoginForm from "@/components/forms/Login";
 import RegisterForm from "@/components/forms/RegisterForm";
 import { NextPageContext } from "next";
-import { getCsrfToken } from "next-auth/react";
+import { getCsrfToken, getProviders } from "next-auth/react";
+
+interface Provider {
+  [key: string]: string;
+}
+
+interface IAuthProps {
+  tab: string;
+  callbackUrl: string;
+  csrfToken: string;
+  providers: Provider[];
+}
 
 export default function auth({
   tab,
   callbackUrl,
   csrfToken,
-}: {
-  tab: string;
-  callbackUrl: string;
-  csrfToken: string;
-}) {
+  providers,
+}: IAuthProps) {
   return (
     <div className="w-full flex items-center justify-center">
       <div className="w-full h-100 flex items-center justify-center">
@@ -23,7 +32,31 @@ export default function auth({
           ) : (
             <RegisterForm />
           )}
+          <div className="w-full flex items-center justify-between px-12">
+            <div className="w-full h-[1px] bg-gray-300"></div>
+            <span className="text-sm uppercase mx-6 text-gray-400">Or</span>
+            <div className="w-full h-[1px] bg-gray-300"></div>
+          </div>
+          <div className="mt-3 grid grid-cols-2 gap-x-4 gap-y-2">
+            {providers.map((provider: Provider) => {
+              if (provider.name == "Credentials") return;
+              return (
+                <SocialButton
+                  key={provider.id}
+                  id={provider.id}
+                  text={
+                    tab == "signup"
+                      ? `Sign up with ${provider.name}`
+                      : `Sign in with ${provider.name}`
+                  }
+                  csrfToken={csrfToken}
+                />
+              );
+            })}
+          </div>
         </div>
+
+        {/*--Background--*/}
         <Background
           image={`"../../auth/${tab == "signup" ? "register" : "login"}.jpg"`}
         />
@@ -40,8 +73,10 @@ export async function getServerSideProps(ctx: NextPageContext) {
     : process.env.NEXTAUTH_URL;
 
   const csrfToken = await getCsrfToken(ctx);
+  const providers = await getProviders();
   return {
     props: {
+      providers: Object.values(providers!),
       tab: JSON.parse(JSON.stringify(tab)),
       callbackUrl,
       csrfToken,
