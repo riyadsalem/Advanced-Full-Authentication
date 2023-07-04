42d00ec021db66ad86c7a04bb29975cab70ecd40
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-07-04 08:54:06 +0300

    Forgot password page and form

---
+++ src/components/forms/Forgot.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import Input from "../inputs/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FiLock, FiMail } from "react-icons/fi";
+import SlideButton from "../buttons/SlideButton";
+import { SubmitHandler } from "react-hook-form/dist/types/form";
+import Link from "next/link";
+
+interface IForgotFormProps {}
+
+const FormSchema = z.object({
+  email: z.string().email("Please enter a valid email adress."),
+});
+
+type FormSchemaType = z.infer<typeof FormSchema>;
+const ForgotForm: React.FunctionComponent<IForgotFormProps> = (props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit: SubmitHandler<FormSchemaType> = async (values) => {};
+
+  return (
+    <div className="w-full px-12 py-4">
+      <h2 className="text-center text-2xl font-bold tracking-wide text-gray-800">
+        Forgot password
+      </h2>
+      <p className="text-center text-sm text-gray-600 mt-2">
+        Sign in instead &nbsp;
+        <Link
+          href="/auth"
+          className="text-blue-600 hover:text-blue-700 hover:underline cursor-pointer"
+        >
+          Sign in
+        </Link>
+      </p>
+      <form className="my-8 text-sm" onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          name="email"
+          label="Email address"
+          type="text"
+          icon={<FiMail />}
+          placeholder="example@emaple.com"
+          register={register}
+          error={errors?.email?.message}
+          disabled={isSubmitting}
+        />
+
+        <SlideButton
+          type="submit"
+          text="Send email"
+          slide_text="Secure"
+          icon={<FiLock />}
+          disabled={isSubmitting}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default ForgotForm;
